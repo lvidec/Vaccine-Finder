7ed937c2f60a71bf9c0a2fa89c0f44c548af7e3f
Leonardo <leonardo.videc.lima@gmail.com>
2021-03-23 17:12:12 +0100

    Initial commit

---
+++ src/test/java/hr/tvz/videc/vaxapp/VaxappApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.videc.vaxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
